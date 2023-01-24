6c983c8ee02ca4f9e1a2272d60153737110e1880
AndrOGennad <listenning@mail.ru>
2023-01-24 22:37:55 +0300

    server startup

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"os/signal"
+	"syscall"
+
+	"github.com/AndrOGennad/go-yandex-devops/internal/server"
+)
+
+func main() {
+	parentCtx := context.Background()
+	ctx, cnl := signal.NotifyContext(parentCtx, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	defer cnl()
+
+	_ = server.Run(ctx)
+	<-ctx.Done()
+}
